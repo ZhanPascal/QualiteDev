c47a1729e6dd6d7518d1c57c7c4f54c3410a6b09
ZhanPascal <pascal.zhan.pro@gmail.com>
2023-04-11 11:18:06 +0200

    Commit tp1

--- src/main/java/com/example/demo/data/Voiture.java
+++ src/main/java/com/example/demo/data/Voiture.java
@@ -7,7 +7,17 @@ import javax.persistence.Id;
 
 @Entity
 public class Voiture {
+
     int id;
+    String marque;
+    int prix;
+
+
+    public Voiture(int id, String marque, int prix) {
+        this.id = id;
+        this.marque = marque;
+        this.prix = prix;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,6 +27,22 @@ public class Voiture {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
     }
 
 }
--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -9,6 +9,14 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
+        Voiture v = new Voiture(1,  "Audi", 15000);
+        Assert.isInstanceOf(Voiture.class, v, "Ce n'est pas une voiture");
+
+        Assert.isTrue(v.getMarque().equals("Audi"), "La voiture n'est pas une Audi");
+        
+        Assert.isTrue(v.getId() == 1,"La voiture n'a pas le bon ID");
+    
+        Assert.isTrue(v.getPrix() == 15000, "La voiture ne coute pas 15000€");
     }
 
 }
